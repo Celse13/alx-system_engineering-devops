7de4618a46b8179c1a7e48b805527bbebfa5572c
Celse13 <117368635+Celse13@users.noreply.github.com>
2024-01-16 23:18:06 +0200

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,9 +9,10 @@ def top_ten(subreddit):
     url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
     response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code == 200:
-        data = response.json()
-        for post in data["data"]["children"]:
-            print(post["data"]["title"])
-    else:
-        print("None")
+    if response.status_code != 200:
+        print(None)
+        return
+
+    data = response.json()
+    for post in data['data']['children']:
+        print(post['data']['title'])
