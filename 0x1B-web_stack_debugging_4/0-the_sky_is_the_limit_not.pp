b53b0eaa971d44d8f045b69d70cd593acc80638e
Celse13 <117368635+Celse13@users.noreply.github.com>
2024-01-25 15:50:41 +0200

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,11 @@
 # Increasing the request limit for nginx web-server
-file_line { 'fix--for-nginx':
-  path  => '/etc/default/nginx',
-  line  => '4096',
-  match => '15',
+exec {'replace':
+  provider => shell,
+  command  => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
 }
 
-# After changes, restart the nginx
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => File_line['fix--for-nginx'],
+exec {'restart':
+  provider => shell,
+  command  => 'service nginx restart',
 }
